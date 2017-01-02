3180f788e09853f25b3822df2c42851d706c63f5
Avraham Lembke <blambi@chebab.com>
2017-01-02 02:18:19 +0100

    Change PID:VID to please linux 4.8+
    1234:5678 is apperently used/registered in the kernel to some
    touchscreen now.

--- keyboards/handwired/pad20/config.h
+++ keyboards/handwired/pad20/config.h
@@ -21,8 +21,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0x1234
-#define PRODUCT_ID      0x5678
+#define VENDOR_ID       0x27db
+#define PRODUCT_ID      0x16c0
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    blambi
 #define PRODUCT         Pad20
